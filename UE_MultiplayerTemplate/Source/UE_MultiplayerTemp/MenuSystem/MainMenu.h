b20554904a0b3577b120479f44f89679c647e199
Matthew Bowen <46889006+S00189001@users.noreply.github.com>
2021-05-03 15:23:11 +0100

    Fully Fixed C++ Directories

--- UE_MultiplayerTemplate/Source/UE_MultiplayerTemp/MenuSystem/MainMenu.h
+++ UE_MultiplayerTemplate/Source/UE_MultiplayerTemp/MenuSystem/MainMenu.h
@@ -4,31 +4,14 @@
 
 #include "CoreMinimal.h"
 #include "Blueprint/UserWidget.h"
-#include "MenuInterface.h"
-
 #include "MainMenu.generated.h"
 
 /**
- *
+ * 
  */
 UCLASS()
 class UE_MULTIPLAYERTEMP_API UMainMenu : public UUserWidget
 {
 	GENERATED_BODY()
-
-protected:
-	virtual bool Initialize();
-
-private:
-	UPROPERTY(meta = (BindWidget))
-		class UButton* Host;
-
-	UPROPERTY(meta = (BindWidget))
-		class UButton* Join;
-
-	UFUNCTION()
-		void HostServer();
-
-	IMenuInterface* MenuInterface;
-
+	
 };
